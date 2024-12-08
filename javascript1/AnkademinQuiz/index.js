298927412a8dbde61ac742a8280e9908fb7eee0d
OzAlbayrak <oz@albayrak.dev>
2024-12-08 15:26:23 +0100

    Add Next question button functionality

--- javascript1/AnkademinQuiz/index.js
+++ javascript1/AnkademinQuiz/index.js
@@ -2,57 +2,96 @@ let quizOnGoing = false;
 let quizStartButton = document.getElementById("quizStartButton");
 let quizAnswerSelection = document.getElementById("quizAnswerSelection");
 quizAnswerSelection.style.display = "none";
-let quizAnswerButton = document.getElementById("quizAnswerButton");
-quizAnswerButton.style.display = "none";
-//const playerAnswer = [{}];
+let quizNextButton = document.getElementById("quizNextButton");
+quizNextButton.style.display = "none";
+let quizEndButton = document.getElementById("quizEndButton");
+quizEndButton.style.display = "none";
+let playerAnswer = [];
 const quizQuestions = [
   {
-    question: "Question 1 is",
-    questionAnswer: [false, false, false, true],
+    questionText: "Question 1 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, false, false, true],
   },
   {
-    question: "Question 2 is",
-    questionAnswer: [false, false, true, false],
+    questionText: "Question 2 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, false, true, false],
   },
   {
-    question: "Question 3 is",
-    questionAnswer: [false, false, true, true],
+    questionText: "Question 3 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, false, true, true],
   },
   {
-    question: "Question 4 is",
-    questionAnswer: [false, true, false, false],
+    questionText: "Question 4 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, true, false, false],
   },
   {
-    question: "Question 5 is",
-    questionAnswer: [false, true, false, true],
+    questionText: "Question 5 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, true, false, true],
   },
   {
-    question: "Question 6 is",
-    questionAnswer: [false, true, true, false],
+    questionText: "Question 6 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, true, true, false],
   },
   {
-    question: "Question 7 is",
-    questionAnswer: [false, true, true, true],
+    questionText: "Question 7 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [false, true, true, true],
   },
   {
-    question: "Question 8 is",
-    questionAnswer: [true, false, false, false],
+    questionText: "Question 8 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [true, false, false, false],
   },
   {
-    question: "Question 9 is",
-    questionAnswer: [true, false, false, true],
+    questionText: "Question 9 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [true, false, false, true],
   },
   {
-    question: "Question 10 is",
-    questionAnswer: [true, false, true, false],
+    questionText: "Question 10 is",
+    questionAnswer: ["a", "b", "c", "d"],
+    questionSolution: [true, false, true, false],
   },
 ];
 
 function startQuiz() {
   quizOnGoing = true;
   playerAnswer = [];
-  console.log(quizStartButton);
   quizStartButton.style.display = "none";
   quizAnswerSelection.style.display = "block";
-  quizAnswerButton.style.display = "block";
+  quizNextButton.style.display = "inline-block";
+  quizEndButton.style.display = "inline-block";
+  console.log(quizQuestions);
+}
+
+function nextQuestion() {
+  playerAnswer.push({
+    answer1: document.getElementById("answer1").checked,
+    answer2: document.getElementById("answer2").checked,
+    answer3: document.getElementById("answer3").checked,
+    answer4: document.getElementById("answer4").checked,
+  });
+  document.getElementById("answer1").checked = false;
+  document.getElementById("answer2").checked = false;
+  document.getElementById("answer3").checked = false;
+  document.getElementById("answer4").checked = false;
+  console.log(playerAnswer.length);
+  console.log(playerAnswer);
+  if (playerAnswer.length > 9) {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  quizStartButton.style.display = "block";
+  quizAnswerSelection.style.display = "none";
+  quizNextButton.style.display = "none";
+  quizEndButton.style.display = "none";
+  console.log("End of game");
 }
